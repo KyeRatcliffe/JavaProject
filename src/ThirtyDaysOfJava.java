3191f77ff4f553b110cee07fbd7fc9e73a8913d5
KyeRatcliffe <Kyeratcliffe98@gmail.com>
2021-03-23 17:11:44 +0000

    initial commit

---
+++ src/ThirtyDaysOfJava.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ThirtyDaysOfJava {
+
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please input your name");
+        String name = userInput.nextLine();
+
+        System.out.println("Thank you: " + name);
+    }
+
+}
