d4553d15c2936fb99dfecf47bdeccb486a535d1f
kyeratcliffe98@gmail.com <Kyeratcliffe98@gmail.com>
2021-03-25 14:25:38 +0000

    Stack Example

--- src/ThirtyDaysOfJava.java
+++ src/ThirtyDaysOfJava.java
@@ -1,26 +1,20 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.lang.reflect.Array;
+import java.util.*;
 
 public class ThirtyDaysOfJava {
 
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) {
 
-        Scanner in = new Scanner(new File("src\\test.txt"));
+        LinkedList<String> names = new LinkedList<String>();
+        names.push("Kye");
+        names.add("John");
+        names.add("sue");
 
-        List<String> students = new ArrayList<String>();
-
-        while (in.hasNextLine()) {
-            students.add(in.nextLine());
-        }
-
-        for(int i = 0; i < students.size(); i++) {
-            System.out.println("name: " + students.get(i));
-        }
-
-        in.close();
+        System.out.println(names.remove(1));
+        System.out.println(names.pop());
+        System.out.println(names.removeLast());
 
     }
 
