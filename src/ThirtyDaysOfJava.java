5c953c14a173429ce08a4f4f2136299d51f77925
kyeratcliffe98@gmail.com <Kyeratcliffe98@gmail.com>
2021-03-26 11:14:52 +0000

    Working with 2D Arrays

--- src/ThirtyDaysOfJava.java
+++ src/ThirtyDaysOfJava.java
@@ -1,26 +1,21 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.lang.reflect.Array;
-import java.util.*;
+
 
 public class ThirtyDaysOfJava {
 
     public static void main(String[] args) {
-        LinkedList<String> names = new LinkedList<String>();
-        names.push("Kye");
-        names.push("John");
-        names.push("Sue");
 
-        ListIterator<String> it = names.listIterator();
-        it.next();
-        it.next();
-        it.add("Kye");
+        int [] [] data = {
+                {4,3,7,2},
+                {5,4,9,2, 6, 4, 3},
+                {7,4,3,6}
+        };
 
-
-        names.add(2, "Peter");
-
-        for(String s: names) {
-            System.out.println(s);
+        for(int i = 0; i < data.length; i++){
+            for(int j = 0; j < data[i].length; j++){
+                System.out.print(data[i][j] + " ");
+            }
+            System.out.println("");
         }
 
     }
